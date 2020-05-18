17611a90aade105b974d97d1eba1467f27b6e5a6
Alfonso Madrid <alfonso.madrid.dev@gmail.com>
2020-05-18 21:51:31 +0200

    Base flask project

---
+++ kairos.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask (__name__)
+
+@app.route('/')
+def stations():
+	return app.response_class(
+		response=open("mocks/stations.json", "r").read(),
+		status=200,
+		mimetype='application/json'
+	)
+
+@app.route('/hefesto/', methods=['POST'])
+def save_register():
+	return app.response_class(
+		status=201,
+	)
