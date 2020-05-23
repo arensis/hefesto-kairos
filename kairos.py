bef6474ec9f32c8898f111995fe7af5f72f8653d
Arensis <alfonso.madrid.dev@gmail.com>
2020-05-23 18:10:05 +0200

    Formated code

--- kairos.py
+++ kairos.py
@@ -1,16 +1,17 @@
 from flask import Flask
-app = Flask (__name__)
 
-@app.route('/')
+app = Flask(__name__)
+
+
+@app.route("/")
 def stations():
-	return app.response_class(
-		response=open("mocks/stations.json", "r").read(),
-		status=200,
-		mimetype='application/json'
-	)
+    return app.response_class(
+        response=open("mocks/stations.json", "r").read(),
+        status=200,
+        mimetype="application/json",
+    )
 
-@app.route('/hefesto/', methods=['POST'])
+
+@app.route("/hefesto/", methods=["POST"])
 def save_register():
-	return app.response_class(
-		status=201,
-	)
+    return app.response_class(status=201)
